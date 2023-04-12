190c5b90c5ac6bbaf924c5de165df3aaca2ff446
thomas clamon <thomas.clamon@gmail.com>
2023-04-12 15:19:49 +0200

    sql

---
+++ src/main/java/com/example/amazoncdan/database/commande.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE [dbo].[Commande](
+    [ID] [int] IDENTITY(1,1) NOT NULL,
+    [id_client] [varchar](50) NOT NULL,
+    [id_produit] [int] NOT NULL,
+    [date] [datetime] NOT NULL,
+    CONSTRAINT [PK_Commande] PRIMARY KEY CLUSTERED
+(
+[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+    ) ON [PRIMARY]
+    GO
+
